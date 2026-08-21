2a427ccf156d645fcbb3eefff190580ba7e9b6c1
Eliya <preciouseliya1@gmail.com>
2026-08-21 10:38:59 +0300

    pull from dev

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // CACHE VERSIONING AND UPDATE LOGIC
 <<<<<<< HEAD
-const CACHE_VERSION = 'v1787037211484'; // Increment this to force update
+const CACHE_VERSION = 'v1787293307295'; // Increment this to force update
 =======
 const CACHE_VERSION = 'v1786425972633'; // Increment this to force update
 >>>>>>> origin/junior-development
